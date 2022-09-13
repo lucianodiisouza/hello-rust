7524c1e6e2fe49eb9e3f3da4a23c90c029249304
Luciano dii Souza <lucianodiisouza@hotmail.com>
2022-09-13 18:35:31 -0300

    testing if

---
+++ src/if.rs
@@ -0,0 +1,10 @@
+fn testingIF() {
+    let number1 = 24;
+    let number2 = 42;
+
+    if number1 > number2 {
+        println!("{} > {}", number1, number2)
+    } else {
+        println!("{} < {}", number1, number2)
+    }
+}
